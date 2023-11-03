1f700892b08ebea24298236871a4be8635b55d98
Mautrak <cenkylmz@hotmail.co.uk>
2023-11-03 03:06:26 +0200

    Script now runs continously and collects data once a day. Checks whether the computer is connected to a working internet connection and if not, waits for a connection. Added requirements.txt.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import re
-from datetime import datetime
 import openpyxl
+import requests
+import time
+from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
@@ -18,61 +20,80 @@ PRICE_COLUMN = 1
 DATE_COLUMN = 2
 SITE_COLUMN = 3
 
+def is_connected():
+    try:
+        requests.get("https://www.google.com", timeout=5)
+        return True
+    except requests.ConnectionError:
+        return False
+
+def wait_until_next_run():
+    now = datetime.now()
+    next_run = datetime(now.year, now.month, now.day, 11, 0) + timedelta(days=1)
+    while datetime.now() < next_run:
+        time.sleep(60)
+
 def get_price(driver, url, xpath):
     driver.get(url)
     price_text = driver.find_element(By.XPATH, xpath).text
     price = re.search(r'[\d\s]+[\.,]?\d*', price_text).group()
-    price = price.replace(' ', '')
-    price = price.replace(',', '.')
+    price = price.replace(' ', '').replace(',', '.')
     return float(price)
 
 def main():
-    today = datetime.now().date()
-    try:
-        workbook = openpyxl.load_workbook(EXCEL_FILE)
-        sheet = workbook.active
-        for row in sheet.iter_rows(values_only=True, min_row=2):  # Skip the header row
-            if datetime.strptime(row[DATE_COLUMN - 1], '%Y-%m-%d').date() == today:
-                print(f"Already checked prices for {today}. Exiting.")
-                return
-    except FileNotFoundError:
-        workbook = openpyxl.Workbook()
-        sheet = workbook.active
-        sheet.append(["Price", "Date", "Site"])
+    while True:
+        today = datetime.now().date()
+        try:
+            workbook = openpyxl.load_workbook(EXCEL_FILE)
+            sheet = workbook.active
+            for row in sheet.iter_rows(values_only=True, min_row=2):
+                if datetime.strptime(row[DATE_COLUMN - 1], '%Y-%m-%d').date() == today:
+                    print(f"Already checked prices for {today}. Waiting until tomorrow.")
+                    wait_until_next_run()
+                    continue
+        except FileNotFoundError:
+            workbook = openpyxl.Workbook()
+            sheet = workbook.active
+            sheet.append(["Price", "Date", "Site"])
+            workbook.save(EXCEL_FILE)
+
+        while not is_connected():
+            print("No internet connection. Waiting...")
+            time.sleep(60)
+
+        options = FirefoxOptions()
+        driver = webdriver.Firefox(options=options)
+
+        print("Connecting to URL1...")
+        try:
+            price1 = get_price(driver, URL1, XPATH1)
+            print(f"Successfully reached URL1. Price found: {price1}")
+        except Exception as e:
+            print(f"Failed to get price from URL1: {e}")
+            price1 = float('inf')
+
+        print("Connecting to URL2...")
+        try:
+            price2 = get_price(driver, URL2, XPATH2)
+            print(f"Successfully reached URL2. Price found: {price2}")
+        except Exception as e:
+            print(f"Failed to get price from URL2: {e}")
+            price2 = float('inf')
+
+        final_price = min(price1, price2)
+        site_used = URL1 if final_price == price1 else URL2
+
+        original_price = 1000.00
+        if final_price <= original_price * 0.7:
+            winsound.Beep(ALARM_FREQUENCY, ALARM_DURATION)
+
+        print("Updating the Excel sheet with the price.")
+        sheet.append([final_price, today.strftime('%Y-%m-%d'), site_used])
         workbook.save(EXCEL_FILE)
 
-    options = FirefoxOptions()
-    driver = webdriver.Firefox(options=options)
-
-    print("Connecting to URL1...")
-    try:
-        price1 = get_price(driver, URL1, XPATH1)
-        print(f"Successfully reached URL1. Price found: {price1}")
-    except Exception as e:
-        print(f"Failed to get price from URL1: {e}")
-        price1 = float('inf')
-
-    print("Connecting to URL2...")
-    try:
-        price2 = get_price(driver, URL2, XPATH2)
-        print(f"Successfully reached URL2. Price found: {price2}")
-    except Exception as e:
-        print(f"Failed to get price from URL2: {e}")
-        price2 = float('inf')
-
-    final_price = min(price1, price2)
-    site_used = URL1 if final_price == price1 else URL2
-
-    original_price = 1000.00
-    if final_price <= original_price * 0.7:
-        winsound.Beep(ALARM_FREQUENCY, ALARM_DURATION)
-
-    print("Updating the Excel sheet with the price.")
-    sheet.append([final_price, today.strftime('%Y-%m-%d'), site_used])
-    workbook.save(EXCEL_FILE)
-
-    driver.quit()
-    print("Done for today.")
+        driver.quit()
+        print("Done for today. Waiting until tomorrow.")
+        wait_until_next_run()
 
 if __name__ == "__main__":
     main()
