a63840e7ea94254e41b796cb0408f6c2326769bc
Mautrak <cenkylmz@hotmail.co.uk>
2023-11-03 03:44:25 +0200

    Commit: Implement Price Tracking Enhancements
    - Rename `PRICE_COLUMN` to `COST_COLUMN` and update header name to "Cost of Victoza".
    - Add currency formatting to display prices in UAH.
    - Calculate and store daily price difference in UAH and percentage.
    - Store friendly site names ("Liki24" and "Tabletki") instead of URLs.
    - Add constants for new columns `DIFFERENCE_COLUMN` and `PERCENTAGE_COLUMN`.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 import openpyxl
+from openpyxl.styles import NamedStyle
 import requests
 import time
 import threading
@@ -17,7 +18,7 @@ XPATH2 = "/html/body/div[2]/main/div[1]/div[1]/div[1]/article/div/div/section[1]
 EXCEL_FILE = "prices.xlsx"
 ALARM_FREQUENCY = 2500
 ALARM_DURATION = 1000
-PRICE_COLUMN = 1
+COST_COLUMN = 1
 DATE_COLUMN = 2
 SITE_COLUMN = 3
 
@@ -46,20 +47,23 @@ def get_price(driver, url, xpath):
     return float(price)
 
 def main():
+    currency_style = NamedStyle(name='currency_style', number_format='#,##0.00 [$₴-422]')
+
     while not stop_event.is_set():
         today = datetime.now().date()
         try:
             workbook = openpyxl.load_workbook(EXCEL_FILE)
             sheet = workbook.active
-            for row in sheet.iter_rows(values_only=True, min_row=2):
-                if datetime.strptime(row[DATE_COLUMN - 1], '%Y-%m-%d').date() == today:
-                    print(f"Already checked prices for {today}. Waiting until tomorrow.")
-                    wait_until_next_run()
-                    continue
+            for col_cells in sheet.iter_cols(min_col=COST_COLUMN, max_col=COST_COLUMN):
+                for cell in col_cells[1:]:  # Skipping the header
+                    cell.style = currency_style
         except FileNotFoundError:
             workbook = openpyxl.Workbook()
             sheet = workbook.active
-            sheet.append(["Cost of Victoza", "Date", "Site"])
+            sheet.append(["Cost of Victoza", "Date", "Site", "Difference (UAH)", "Difference (%)"])
+            for col_cells in sheet.iter_cols(min_col=COST_COLUMN, max_col=COST_COLUMN):
+                for cell in col_cells[1:]:
+                    cell.style = currency_style
             workbook.save(EXCEL_FILE)
 
         while not is_connected():
@@ -88,12 +92,21 @@ def main():
         final_price = min(price1, price2)
         site_used = "Liki24" if final_price == price1 else "Tabletki"
 
-        original_price = 4185.00
+        diff_uah = 0
+        diff_percentage = 0
+        last_row = sheet.max_row
+
+        if last_row > 1:
+            yesterday_price = sheet.cell(row=last_row, column=COST_COLUMN).value
+            diff_uah = final_price - yesterday_price
+            diff_percentage = (diff_uah / yesterday_price) * 100
+
+        original_price = 1000.00
         if final_price <= original_price * 0.7:
             winsound.Beep(ALARM_FREQUENCY, ALARM_DURATION)
 
         print("Updating the Excel sheet with the price.")
-        sheet.append([final_price, today.strftime('%Y-%m-%d'), site_used])
+        sheet.append([final_price, today.strftime('%Y-%m-%d'), site_used, diff_uah, diff_percentage])
         workbook.save(EXCEL_FILE)
 
         driver.quit()
